1c72b9b1c16389eb23851f8dff8c1080ee8d2f8f
Esdras Franca <esdras.franca.85@gmail.com>
2026-02-23 13:06:30 -0300

    Inclusão dos arquivos de layout e criação das rotas básicas.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [MainController::class, 'home']);
+Route::get('/gerar-exercicios', [MainController::class, 'gerarExercicios']);
+Route::get('/imprimir-exercicios', [MainController::class, 'imprimirExercicios']);
+Route::get('/exportar-exercicios', [MainController::class, 'exportarExercicios']);
